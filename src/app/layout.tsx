d1f10df98a62803ad8928e512ddb431628953d4e
Lars Moser <moser.lars@mail.de>
2024-05-16 07:33:16 -0500

    feat: add video scrub logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mosr Project Starter",
+  title: "Mosr Video Mouse Scrub",
   description: "Created by Lars Mosr",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import dynamic from "next/dynamic";
+
+const StickyCursor = dynamic(() => import("@/components/sticky-cursor"), {
+  ssr: false,
+});
+
+const VideoScrub = dynamic(() => import("@/components/video-mouse-scrub"), {
+  ssr: false,
+});
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="relative h-screen">
+      <StickyCursor />
+      <VideoScrub />
+    </main>
+  );
 }
